240bbea49d5111343285688ce4fba3b522b3039b
MaheshGowda47 <aetherix.in@gmail.com>
2023-12-15 21:05:29 +0530

    ---

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Airline Passenger Satisfaction",
+    author="Mahesh Gowda",
+    author_email="maheshbuddappa30@gmail.com",
+    version="0.0.1",
+    packages=find_packages,
+)
+
